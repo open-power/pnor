a7f9b25eb1e7fac405bc1a54597cfc812a07fb3f
Bill Hoffa <wghoffa@us.ibm.com>
2014-06-30 22:51:52 -0500

    Initial commit.

---
+++ create_pnor_image.pl
@@ -0,0 +1,139 @@
+#!/usr/bin/perl
+
+use strict;
+use File::Basename;
+
+my $program_name = File::Basename::basename $0;
+my $outdir = "";
+my $scratch_dir = "";
+my $pnor_data_dir = "";
+my $pnor_filename = "";
+my $payload = "";
+my $hb_image_dir = "";
+my $xml_layout_file = "";
+my $targeting_binary_filename = "";
+my $sbec_binary_filename = "";
+my $sbe_binary_filename = "";
+
+while (@ARGV > 0){
+    $_ = $ARGV[0];
+    chomp($_);
+    $_ = &trim_string($_);
+    if (/^-h$/i || /^-help$/i || /^--help$/i){
+        #print help content
+        usage();
+        exit 0;
+    }
+    elsif (/^-scratch_dir/i){
+        $scratch_dir = $ARGV[1] or die "Bad command line arg given: expecting a scratch dir path.\n";
+        shift;
+    }
+    elsif (/^-outdir/i){
+        $outdir = $ARGV[1] or die "Bad command line arg given: expecting a directory for output data.\n";
+        shift;
+    }
+    elsif (/^-pnor_data_dir/i){
+        $pnor_data_dir = $ARGV[1] or die "Bad command line arg given: expecting a directory containing pnor data.\n";
+        shift;
+    }
+    elsif (/^-pnor_filename/i){
+        $pnor_filename = $ARGV[1] or die "Bad command line arg given: expecting a pnor filename.\n";
+        shift;
+    }
+    elsif (/^-hb_image_dir/i){
+        $hb_image_dir = $ARGV[1] or die "Bad command line arg given: expecting an hb image dir path.\n";
+        shift;
+    }
+    elsif (/^-xml_layout_file/i){
+        $xml_layout_file = $ARGV[1] or die "Bad command line arg given: expecting an xml layout file.\n";
+        shift;
+    }
+    elsif (/^-payload/i){
+        $payload = $ARGV[1] or die "Bad command line arg given: expecting a filepath to payload binary file.\n";
+        shift;
+    }
+    elsif (/^-targeting_binary_filename/i){
+        $targeting_binary_filename = $ARGV[1] or die "Bad command line arg given: expecting a targeting binary filename.\n";
+        shift;
+    }
+    elsif (/^-sbe_binary_filename/i){
+        $sbe_binary_filename = $ARGV[1] or die "Bad command line arg given: expecting an sbe binary filename.\n";
+        shift;
+    }
+    elsif (/^-sbec_binary_filename/i){
+        $sbec_binary_filename = $ARGV[1] or die "Bad command line arg given: expecting an sbec binary filename.\n";
+        shift;
+    }
+    else {
+        print "Unrecognized command line arg: $_ \n";
+        print "To view all the options and help text run \'$program_name -h\' \n";
+        exit 1;
+    }
+    shift;
+}
+
+if ($outdir eq "") {
+    die "-outdir <path_to_directory_for_output_files> is a required command line variable. Please run again with this parameter.\n";
+}
+
+print "scratch_dir = $scratch_dir\n";
+print "pnor_data_dir = $pnor_data_dir\n";
+
+my $build_pnor_command = "$hb_image_dir/buildpnor.pl";
+$build_pnor_command .= " --pnorOutBin $pnor_filename --pnorLayout $xml_layout_file";
+$build_pnor_command .= " --binFile_HBD $scratch_dir/$targeting_binary_filename";
+$build_pnor_command .= " --binFile_SBE $scratch_dir/$sbe_binary_filename";
+$build_pnor_command .= " --binFile_SBEC $scratch_dir/$sbec_binary_filename";
+$build_pnor_command .= " --binFile_WINK $scratch_dir/p8.ref_image.hdr.bin.ecc";
+$build_pnor_command .= " --binFile_HBB $scratch_dir/hostboot.header.bin.ecc";
+$build_pnor_command .= " --binFile_HBI $scratch_dir/hostboot_extended.header.bin.ecc";
+$build_pnor_command .= " --binFile_HBRT $scratch_dir/hostboot_runtime.header.bin.ecc";
+$build_pnor_command .= " --binFile_HBEL $scratch_dir/hbel.bin.ecc";
+$build_pnor_command .= " --binFile_GUARD $scratch_dir/guard.bin.ecc";
+$build_pnor_command .= " --binFile_PAYLOAD $payload";
+$build_pnor_command .= " --binFile_NVRAM $scratch_dir/nvram.bin.ecc";
+$build_pnor_command .= " --binFile_MVPD $scratch_dir/mvpd_fill.bin.ecc";
+$build_pnor_command .= " --binFile_DJVPD $scratch_dir/djvpd_fill.bin.ecc";
+$build_pnor_command .= " --binFile_CVPD $scratch_dir/cvpd.bin.ecc";
+$build_pnor_command .= " --fpartCmd \"fpart\"";
+$build_pnor_command .= " --fcpCmd \"fcp\"";
+print "###############################";
+run_command("$build_pnor_command");
+
+#END MAIN
+#-------------------------------------------------------------------------
+sub usage {
+
+
+print <<"ENDUSAGE";
+
+
+ENDUSAGE
+
+;
+}
+
+
+sub parse_config_file {
+
+}
+
+
+#trim_string takes one string as input, trims leading and trailing whitespace
+# before returning that string
+sub trim_string {
+    my $str = shift;
+    $str =~ s/^\s+//;
+    $str =~ s/\s+$//;
+    return $str;
+}
+
+sub run_command {
+    my $command = shift;
+    print "$command\n";
+    my $rc = system($command);
+    if ($rc !=0 ){
+        die "Error running command: $command. Nonzero return code of ($rc) returned.\n";
+    }
+    return $rc;
+}
---
+++ update_image_palmetto.pl
@@ -0,0 +1,131 @@
+#!/usr/bin/perl
+
+use strict;
+use File::Basename;
+
+#my $ecc_tool_dir = "/opt/mcp/shared/fr_FLD8-1-20140528/opt/fsp/usr/bin"; #wh_todo
+
+my $op_target_dir = "";
+my $hb_image_dir = "";
+my $scratch_dir = "";
+my $hb_binary_dir = "";;
+
+while (@ARGV > 0){
+    $_ = $ARGV[0];
+    chomp($_);
+    $_ = &trim_string($_);
+    if (/^-h$/i || /^-help$/i || /^--help$/i){
+        usage(); #print help content
+        exit 0;
+    }
+    elsif (/^-op_target_dir/i){
+        $op_target_dir = $ARGV[1] or die "Bad command line arg given: expecting a config type.\n";
+        shift;
+    }
+    elsif (/^-hb_image_dir/i){
+        $hb_image_dir = $ARGV[1] or die "Bad command line arg given: expecting a config type.\n";
+        shift;
+    }
+    elsif (/^-scratch_dir/i){
+        $scratch_dir = $ARGV[1] or die "Bad command line arg given: expecting a config type.\n";
+        shift;
+    }
+    elsif (/^-hb_binary_dir/i){
+        $hb_binary_dir = $ARGV[1] or die "Bad command line arg given: expecting a config type.\n";
+        shift;
+    }
+    else {
+        print "Unrecognized command line arg: $_ \n";
+        #print "To view all the options and help text run \'$program_name -h\' \n";
+        exit 1;
+    }
+    shift;
+}
+
+#Pad Targeting binary to 4k page size, then add ECC data
+run_command("dd if=$op_target_dir/PALMETTO_HB.targeting.bin of=$scratch_dir/PALMETTO_HB.targeting.bin ibs=4k conv=sync");
+run_command("ecc --inject $scratch_dir/PALMETTO_HB.targeting.bin --output $scratch_dir/PALMETTO_HB.targeting.bin --p8");
+
+run_command("echo \"00000000001800000000000008000000000000000007EF80\" | xxd -r -ps - $scratch_dir/sbe.header");
+run_command("echo -en VERSION\\\\0 > $scratch_dir/hostboot.sha.bin");
+run_command("sha512sum $hb_image_dir/img/hostboot.bin | awk \'{print \$1}\' | xxd -pr -r >> $scratch_dir/hostboot.sha.bin");
+run_command("dd if=$scratch_dir/hostboot.sha.bin of=$scratch_dir/secureboot.header ibs=4k conv=sync");
+run_command("dd if=/dev/zero of=$scratch_dir/hbb.footer count=1 bs=128K");
+run_command("cat $scratch_dir/sbe.header $scratch_dir/secureboot.header $hb_image_dir/img/hostboot.bin $scratch_dir/hbb.footer > $scratch_dir/hostboot.stage.bin");
+run_command("head -c 524288 $scratch_dir/hostboot.stage.bin > $scratch_dir/hostboot.header.bin");
+
+run_command("ecc --inject $hb_image_dir/img/hostboot.bin --output $scratch_dir/hostboot.bin.ecc --p8");
+run_command("ecc --inject $scratch_dir/hostboot.header.bin --output $scratch_dir/hostboot.header.bin.ecc --p8");
+run_command("dd if=$hb_image_dir/img/hostboot_extended.bin of=$scratch_dir/hostboot_extended.bin.pad ibs=4k count=1280 conv=sync");
+run_command("ecc --inject $scratch_dir/hostboot_extended.bin.pad --output $scratch_dir/hostboot_extended.bin.ecc --p8");
+
+run_command("echo -en VERSION\\\\0 > $scratch_dir/hostboot_runtime.sha.bin");
+run_command("sha512sum $hb_image_dir/img/hostboot_runtime.bin | awk \'{print \$1}\' | xxd -pr -r >> $scratch_dir/hostboot_runtime.sha.bin");
+run_command("dd if=$scratch_dir/hostboot_runtime.sha.bin of=$scratch_dir/hostboot.temp.bin ibs=4k conv=sync");
+run_command("cat $hb_image_dir/img/hostboot_runtime.bin >> $scratch_dir/hostboot.temp.bin");
+run_command("dd if=$scratch_dir/hostboot.temp.bin of=$scratch_dir/hostboot_runtime.header.bin ibs=2048K conv=sync");
+run_command("ecc --inject $scratch_dir/hostboot_runtime.header.bin --output $scratch_dir/hostboot_runtime.header.bin.ecc --p8");
+
+run_command("echo -en VERSION\\\\0 > $scratch_dir/hostboot_extended.sha.bin");
+run_command("sha512sum $hb_image_dir/img/hostboot_extended.bin | awk \'{print \$1}\' | xxd -pr -r >> $scratch_dir/hostboot_extended.sha.bin");
+run_command("dd if=$scratch_dir/hostboot_extended.sha.bin of=$scratch_dir/hostboot.temp.bin ibs=4k conv=sync");
+run_command("cat $hb_image_dir/img/hostboot_extended.bin >> $scratch_dir/hostboot.temp.bin");
+run_command("dd if=$scratch_dir/hostboot.temp.bin of=$scratch_dir/hostboot_extended.header.bin ibs=5120k conv=sync");
+run_command("ecc --inject $scratch_dir/hostboot_extended.header.bin --output $scratch_dir/hostboot_extended.header.bin.ecc --p8");
+
+#Create blank binary file for HB Errorlogs (HBEL) Partition
+run_command("dd if=/dev/zero bs=128K count=1 | tr \"\\000\" \"\\377\" > $scratch_dir/hostboot.temp.bin");
+run_command("ecc --inject $scratch_dir/hostboot.temp.bin --output $scratch_dir/hbel.bin.ecc --p8");\
+
+#Create blank binary file for GUARD Data (GUARD) Partition
+run_command("dd if=/dev/zero bs=16K count=1 | tr \"\\000\" \"\\377\" > $scratch_dir/hostboot.temp.bin");
+run_command("ecc --inject $scratch_dir/hostboot.temp.bin --output $scratch_dir/guard.bin.ecc --p8");
+
+#Create blank binary file for NVRAM Data (NVRAM) Partition
+run_command("dd if=/dev/zero bs=512K count=1 | tr \"\\000\" \"\\377\" > $scratch_dir/hostboot.temp.bin");
+run_command("ecc --inject $scratch_dir/hostboot.temp.bin --output $scratch_dir/nvram.bin.ecc --p8");
+
+#Create blank binary file for MVPD Partition
+run_command("dd if=/dev/zero bs=512K count=1 | tr \"\\000\" \"\\377\" > $scratch_dir/hostboot.temp.bin");
+run_command("ecc --inject $scratch_dir/hostboot.temp.bin --output $scratch_dir/mvpd_fill.bin.ecc --p8");
+
+#Create blank binary file for DJVPD Partition
+run_command("dd if=/dev/zero bs=256K count=1 | tr \"\\000\" \"\\377\" > $scratch_dir/hostboot.temp.bin");
+run_command("ecc --inject $scratch_dir/hostboot.temp.bin --output $scratch_dir/djvpd_fill.bin.ecc --p8");
+
+#Add ECC Data to CVPD Data (CVPD) Partition
+run_command("ecc --inject $hb_binary_dir/cvpd.bin --output $scratch_dir/cvpd.bin.ecc --p8");
+
+#Copy Binary Data files for consistency
+run_command("cp $hb_binary_dir/palmetto_sbec_pad.img.ecc $scratch_dir/");
+run_command("cp $hb_binary_dir/palmetto_sbe.img.ecc $scratch_dir/");
+run_command("cp $hb_binary_dir/p8.ref_image.hdr.bin.ecc $scratch_dir/");
+
+#END MAIN
+#-------------------------------------------------------------------------
+
+
+
+
+
+############# HELPER FUNCTIONS #################################################
+# Function to first print, and then run a system command, erroring out if the
+#  command does not complete successfully
+sub run_command {
+    my $command = shift;
+    print "$command\n";
+    my $rc = system($command);
+    if ($rc !=0 ){
+        die "Error running command: $command. Nonzero return code of ($rc) returned.\n";
+    }
+    return $rc;
+}
+
+# Function to remove leading and trailing whitespeace before returning that string
+sub trim_string {
+    my $str = shift;
+    $str =~ s/^\s+//;
+    $str =~ s/\s+$//;
+    return $str;
+}
+
